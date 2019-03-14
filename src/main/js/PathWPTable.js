72b9e31be703cae76bb0758f12d956d63c164245
Mohamed Karray <mohamed.karray.sr@gmail.com>
2019-03-14 17:36:19 +0100

    fix i18n for the table

--- src/main/js/PathWPTable.js
+++ src/main/js/PathWPTable.js
@@ -3,15 +3,19 @@
 import React from "react";
 import type { PathWP } from "./PathWP";
 import PathWPTableRow from "./PathWPTableRow";
+import { translate } from "react-i18next";
 
 type Props = {
   permissions: PathWP[],
   // permissionListChanged: (permissions: PathWP[]) => void
-  onDelete: PathWP => void
+  onDelete: PathWP => void,
+  // context prop
+  t: string => string
 };
 
 class PathWPTable extends React.Component<Props> {
   render() {
+    const { t } = this.props;
     const tableRows = this.props.permissions.map(pathWP => {
       return (
         <>
@@ -27,11 +31,11 @@ class PathWPTable extends React.Component<Props> {
     return (
       <table className="card-table table is-hoverable is-fullwidth">
         <thead>
-          <th>Path</th>
-          <th>Name</th>
-          <th>Type</th>
-          <th>Group</th>
-          <th>Delete</th>
+        <th>{t("scm-pathwp-plugin.table.path")}</th>
+        <th>{t("scm-pathwp-plugin.table.name")}</th>
+        <th>{t("scm-pathwp-plugin.table.type")}</th>
+        <th>{t("scm-pathwp-plugin.table.group")}</th>
+        <th>{t("scm-pathwp-plugin.table.delete")}</th>
         </thead>
         {tableRows}
       </table>
@@ -39,4 +43,4 @@ class PathWPTable extends React.Component<Props> {
   }
 }
 
-export default PathWPTable;
+export default translate("plugins")( PathWPTable);
